240b7b9b786f1f6088f67fdeb839361049008dda
Kieu <kieungothekieu@gmail.com>
2021-01-21 18:42:34 +0700

    hoan thanh 3 trang, chua hoan thanh css chuyen trang

---
+++ script.js
@@ -0,0 +1,6 @@
+$(document).ready(function() {
+    var colors  = ["red","orange","pink","chocolate","blue","brown","yellow","deepskyblue","purple","violet","silver","navy","gray","beige","tan"];
+    colors.forEach(element => {
+        
+    });
+})
