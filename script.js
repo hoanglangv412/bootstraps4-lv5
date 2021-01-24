4072fb9f866dce710706351b05836862ad227b21
kieu <kieungothekieu@gmail.com>
2021-01-24 23:38:58 +0700

    hoan thanh xong giao dien bootstrap lv4

--- script.js
+++
@@ -1,6 +0,0 @@
-$(document).ready(function() {
-    var colors  = ["red","orange","pink","chocolate","blue","brown","yellow","deepskyblue","purple","violet","silver","navy","gray","beige","tan"];
-    colors.forEach(element => {
-        
-    });
-})
